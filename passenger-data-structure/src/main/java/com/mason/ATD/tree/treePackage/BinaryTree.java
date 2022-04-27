27050e34a1aac26ca7023cdb60d6d5fd810b7cfb
shi <1547394239@qq.com>
2022-04-27 23:13:28 +0800

    BinaryTree--preOrderTraverse and inorderTraverse

--- passenger-data-structure/src/main/java/com/mason/ATD/tree/treePackage/BinaryTree.java
+++ passenger-data-structure/src/main/java/com/mason/ATD/tree/treePackage/BinaryTree.java
@@ -1,8 +1,10 @@
 package com.mason.ATD.tree.treePackage;
 
-import com.mason.ATD.chapter01.BagInterface;
+import com.mason.ATD.chapter05.LinkedStack;
+import com.mason.ATD.chapter05.StackInterface;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * @author Mason
@@ -101,9 +103,80 @@ public class BinaryTree<T> implements BinaryTreeInterface<T> {
         return root;
     }
 
+
+    //递归实现中序遍历
+    public void inorderTraverse() {
+        inorderTraverse(root);
+    }
+
+    //对于递归处理子树的方法，它需要子树的根作为参数，为了让递归方法是私有的
+    // 且从一个无参数的共有方法来调用它。
+    //为了简单起见，我们只显示数据，尽管ADT的类一般不应执行输入和输出。
+    private void inorderTraverse(BinaryNode<T> node) {
+        if (node != null) {
+            inorderTraverse(node.getLeftChild());
+            System.out.println(node.getData());
+            inorderTraverse(node.getRightChild());
+        }
+    }
+
+    /**
+     * 实现前序遍历
+     */
+    public void preorderTraverse() {
+        preorderTraverse(root);
+    }
+
+    private void preorderTraverse(BinaryNode<T> node) {
+        if (node != null) {
+            System.out.println(node.getData());
+            preorderTraverse(node.getLeftChild());
+            preorderTraverse(node.getRightChild());
+        }
+    }
+
     @Override
     public Iterator<T> getPreorderIterator() {
-        return null;
+        return new PreorderIterator();
+    }
+
+    private class PreorderIterator implements Iterator<T> {
+        private StackInterface<BinaryNode<T>> nodeStack;
+        private BinaryNode<T> currentNode;
+
+        public PreorderIterator() {
+            nodeStack = new LinkedStack<>();
+            currentNode = root;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !nodeStack.isEmpty() || currentNode != null;
+        }
+
+        @Override
+        public T next() {
+            BinaryNode<T> nextNode = null;
+            if (currentNode != null)
+                nodeStack.push(currentNode);
+            // Visit leftmost node, then traverse its right subtree
+            if (!nodeStack.isEmpty()) {
+                nextNode = nodeStack.pop();
+                currentNode = nextNode.getRightChild();
+                if (currentNode != null)
+                    nodeStack.push(currentNode);
+                currentNode = nextNode.getLeftChild();
+            } // end if
+            else
+                throw new NoSuchElementException();
+
+            return nextNode.getData();
+        }
+
+        @Override
+        public void remove() {
+            Iterator.super.remove();
+        }
     }
 
     @Override
@@ -113,11 +186,86 @@ public class BinaryTree<T> implements BinaryTreeInterface<T> {
 
     @Override
     public Iterator<T> getInorderIterator() {
-        return null;
+        return new InorderIterator();
+    }
+
+    //中序遍历的迭代方法,通过迭代器iterator的hasNext(),next()进行逻辑的分解
+    public void iteratorInorderTraverse() {
+        StackInterface<BinaryNode<T>> nodeStack = new LinkedStack<>();
+        BinaryNode<T> currentNode = root;
+        while (!nodeStack.isEmpty() || (currentNode != null)) {
+            // Find leftmost mode with no left child
+            while (currentNode != null) {
+                nodeStack.push(currentNode);
+                currentNode = currentNode.getLeftChild();
+            }//Visit leftmost node, then traverse its right subtree
+            if (!nodeStack.isEmpty()) {
+                BinaryNode<T> nextNode = nodeStack.pop();
+                currentNode = nextNode.getRightChild();
+            }
+        }
+    }
+
+    public void iteratorPreorderTraverse() {
+        StackInterface<BinaryNode<T>> nodeStack = new LinkedStack<>();
+        BinaryNode<T> currentNode = root;
+        while (!nodeStack.isEmpty() || (currentNode != null)) {
+            if (currentNode != null)
+                nodeStack.push(currentNode);
+            if (!nodeStack.isEmpty()) {
+                BinaryNode<T> nextNode = nodeStack.pop();
+                assert nextNode != null;
+                System.out.println(nextNode.getData());
+                currentNode = nextNode.getRightChild();
+                if (currentNode != null)
+                    nodeStack.push(currentNode);
+                currentNode = nextNode.getLeftChild();
+            }
+        }
+
+
     }
 
     @Override
     public Iterator<T> getLevelOrderIterator() {
         return null;
+    }
+
+    private class InorderIterator implements Iterator<T> {
+        private StackInterface<BinaryNode<T>> nodeStack;
+        private BinaryNode<T> currentNode;
+
+        public InorderIterator() {
+            nodeStack = new LinkedStack<>();
+            currentNode = root;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !nodeStack.isEmpty() || (currentNode != null);
+        }
+
+        @Override
+        public T next() {
+            BinaryNode<T> nextNode = null;
+            //Find leftmost node with no left child
+            while (currentNode != null) {
+                nodeStack.push(currentNode);
+                currentNode = currentNode.getLeftChild();
+            }
+            //Get leftmost node, then move to its right subtree
+            if (!nodeStack.isEmpty()) {
+                nextNode = nodeStack.pop();
+                currentNode = nextNode.getRightChild();
+            } else
+                throw new NoSuchElementException();
+
+            return nextNode.getData();
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
     }
 }
